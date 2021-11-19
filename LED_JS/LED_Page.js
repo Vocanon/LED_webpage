1e2f9b9d2343bc558e7448b82c64b63151322777
Vocanon <Vocanon2000@gmail.com>
2021-11-19 16:58:38 -0600

    js

--- LED_JS/LED_Page.js
+++ LED_JS/LED_Page.js
@@ -1,14 +1,52 @@
 window.addEventListener("load", setup);
 
 function setup() {
-    deletedefaults();
+    document.getElementById("testers").addEventListener("click", gathers);
     document.getElementsByTagName("form")[0].addEventListener("submit", gatherinfo);
+
 
 }
 
-function gatherinfo {
-    var name = document.getElementById('Name').value;
-    var emails = document.getElementById('Email').value;
-    var Programname = document.getElementById('PName').value;
+function gatherinfo() {
+    var exporter = new FormData(document.getElementById('form'));
+    console.log(exporter + "hello");
+    var urls = "";
 
+
+
+}
+
+function gathers() {
+    var doc = new FormData(document.getElementById('led_f'))
+
+    var xml = new XMLHttpRequest();
+    xml.onreadystatechange = responder;
+    var name = document.getElementById("Name").value;
+    var emails = document.getElementById("Email").value;
+    var Programname = document.getElementById("PName").value;
+    doc.append(name);
+    doc.append(emails);
+    var file = document.getElementById("Prog").value;
+    var j = name;
+    xml.open('Post', 'http://172.16.42.116:4006/create', true);
+    xml.send(doc);
+
+
+
+    document.getElementById("test").innerHTML = file;
+    console.log(file, Programname, emails, name);
+
+    function responder() {
+        if (xml.readyState === XMLHttpRequest.DONE) {
+            var status = xml.status;
+            if (status === 0 || (status >= 200 && status < 400)) {
+                // The request has been completed successfully
+
+                console.log(xml.responseText);
+            } else {
+                // Oh no! There has been an error with the request!
+                console.log("error");
+            }
+        }
+    }
 }
